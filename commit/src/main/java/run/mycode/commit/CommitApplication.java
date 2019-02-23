34bb2bb4a013ba49b84786faab6d128b39f4fb68
Brian Dahlem <dahlem.brian@cusd80.com>
2019-02-23 01:44:48 +0000

    initial commit

---
+++ commit/src/main/java/run/mycode/commit/CommitApplication.java
@@ -0,0 +1,13 @@
+package run.mycode.commit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitApplication.class, args);
+	}
+
+}
