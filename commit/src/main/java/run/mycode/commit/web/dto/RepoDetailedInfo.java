ac77bbde09e9ee834832b85815cafe3eba556375
mrbdahlem <dahlem.brian@cusd80.com>
2019-03-26 20:43:11 -0700

    fixed repodetailedinfo equals/hash

--- commit/src/main/java/run/mycode/commit/web/dto/RepoDetailedInfo.java
+++ commit/src/main/java/run/mycode/commit/web/dto/RepoDetailedInfo.java
@@ -3,10 +3,12 @@ package run.mycode.commit.web.dto;
 import java.io.IOException;
 import java.util.Date;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.kohsuke.github.GHRepository;
 
 @Data
+@EqualsAndHashCode(callSuper=true)
 @NoArgsConstructor
 public class RepoDetailedInfo extends RepoInfo {
     private Date lastUpdated;
